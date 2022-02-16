a3ba2f6762e69504b4f43ee8d02c95066dd56d01
kleim <kleimenov.mi@phystech.edu>
2022-02-17 00:41:22 +0300

    Monkey-program, file-writing added

---
+++ wk1/cmake-build-debug/CMakeFiles/wk1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wk1.dir/5.cpp.obj"
+  "libwk1.dll.a"
+  "wk1.exe"
+  "wk1.exe.manifest"
+  "wk1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wk1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ wk2/cmake-build-debug/CMakeFiles/wk2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wk2.dir/a.cpp.obj"
+  "libwk2.dll.a"
+  "wk2.exe"
+  "wk2.exe.manifest"
+  "wk2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wk2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
